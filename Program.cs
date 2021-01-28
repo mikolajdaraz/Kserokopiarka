0a40ce41ba24cf919949a17cd9a01319cdd52e0c
Mikolaj <mikolajdara@gmail.com>
2021-01-28 23:41:24 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Zadanie1;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xerox = new Copier();
+            xerox.PowerOn();
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            xerox.Print(in doc1);
+
+            IDocument doc2;
+            xerox.Scan(out doc2);
+
+            xerox.ScanAndPrint();
+            System.Console.WriteLine(xerox.Counter);
+            System.Console.WriteLine(xerox.PrintCounter);
+            System.Console.WriteLine(xerox.ScanCounter);
+        }
+    }
+}
